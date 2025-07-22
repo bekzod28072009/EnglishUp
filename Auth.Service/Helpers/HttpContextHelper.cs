84130c1053e95806e5a67b4057062b2cbf9ea222
Keldiyorov Bekzod <bekhzodkeldiyorov@gmail.com>
2025-07-22 23:23:23 +0500

    Added Settings

---
+++ Auth.Service/Helpers/HttpContextHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Auth.Service.Helpers;
+
+public class HttpContextHelper
+{
+    public static IHttpContextAccessor Accessor { get; set; }
+    public static HttpContext HttpContext => Accessor?.HttpContext;
+    public static IHeaderDictionary ResponseHeaders => HttpContext?.Response?.Headers;
+    public static long? UserId => GetUserId();
+    public static string UserRole => GetUserRoul();
+    public static List<string> UserPermission => GetUserPermission();
+
+    private static long? GetUserId()
+    {
+        string value = HttpContext?.User?.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier)?.Value;
+
+        bool canParse = long.TryParse(value, out long id);
+        return canParse ? id : null;
+    }
+
+    private static string GetUserRoul()
+    {
+        string value = HttpContext?.User?.Claims.FirstOrDefault(p => p.Type == ClaimTypes.Role)?.Value;
+        return value;
+    }
+
+    private static List<string> GetUserPermission()
+    {
+        var permissionsJson = HttpContext?.User?.Claims
+            .FirstOrDefault(p => p.Type == "Permissions")?.Value;
+
+        if (string.IsNullOrEmpty(permissionsJson))
+        {
+            return new List<string>();
+        }
+
+        // Agar JSON formatida bo'lmasa, vergul bilan ajratamiz
+        if (!permissionsJson.StartsWith("["))
+        {
+            return permissionsJson.Split(',').ToList();
+        }
+
+        var permissions = JsonSerializer.Deserialize<List<string>>(permissionsJson);
+        return permissions ?? new List<string>();
+    }
+}
